19fa8a2e0c3317d82b70bab8482a58e89dc9c3fe
Apoorva <apoorva.choudhary@iiitb.org>
2021-02-09 00:41:50 +0530

    add app.js file

---
+++ app.js
@@ -0,0 +1,16 @@
+const name = 'James';
+const heyas = 'lala';
+const person = { first: heyas };
+const arr = [   '1', '2'];
+
+console.log(person);
+
+if (name) {
+
+}
+
+function hey(fName) {
+  console.log(`Hello linting, ${fName}`);
+}
+
+hey(name);
